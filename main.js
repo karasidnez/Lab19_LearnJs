e4e669db26bce3bd9d899e8084e78885fb29c3f0
mrultimatum07-alt <mrultimatum07@gmail.com>
2026-03-10 12:55:52 +0300

    Step 5: loops in JavaScript

---
+++ main.js
@@ -0,0 +1,58 @@
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+// let a = 0;
+// for (let i = 1; i <= 10; i++) {
+//   a += i;
+// }
+// console.log(a);
+
+// let count = 0;
+// while (count < 3) {
+//   console.log("Count:", count);
+//   count++;
+// }
+
+// let number = 5;
+// while (number != 0) {
+//   console.log(number);
+//   number--;
+// }
+
+// let doValue = 0;
+// do {
+//   console.log("Value:", doValue);
+//   doValue++;
+// } while (doValue < 3);
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 5) {
+//     i++;
+//   } else if (i == 8) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 3; i++){
+//     for (let j = 1; j <= 3; j++) {
+//         console.log(`i = ${i}, j = ${j}`);
+//     }
+// }
+// let a = "*";
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= 4; j++) {
+//     console.log(a);
+//     a += "*";
+//   }
+// }
+
+
